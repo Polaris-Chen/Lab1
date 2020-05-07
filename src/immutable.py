e7217b6954795ef6b0cde511ec5921c11a1d6c9d
polarischen <409744612@qq.com>
2020-05-07 13:32:24 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/mutable.py
    #	src/mutable_test.py

--- src/immutable.py
+++ src/immutable.py
@@ -1,88 +1,118 @@
 #Author: ChenZhengHui
+
 class HashMap(object):
 
     __a=()
+    __hashmaplength=0
 
     def geta(self):
-        return a
+        return self.__a
+    def getLength(self):
+        return self.__hashmaplength
 
-    def __init__(self):
+    def __init__(self,hashmaplength):
+
+        self.__hashmaplength=hashmaplength
         temp = []
-        for i in range(10):
+
+        for i in range(hashmaplength):
             temp.append(None)
 
-        self.a = tuple(temp)
+        self.__a = tuple(temp)
 
     def size(self):
         if self == None:
             return 0
         num = 0
         #print(len(self.a))
-        for i in range(10):
-            if self.a[i] is not None:
+        for i in range(self.getLength()):
+            if self.__a[i] is not None:
                 num += 1
         return num
 
     def clear(self):
-        self.a=a=(None,None,None,None,None,None,None,None,None,None)
+        temp=list(self.geta())
+        #print(len(temp))
+        for i in range(self.getLength()):
+           temp[i]=None
+        self.__a=tuple(temp)
 
     def add(self, value):
-        index = value % len(self.a)
+        index = value % len(self.__a)
 
-        while self.a[index] is not None:
-            if index < 9:
+        while self.__a[index] is not None:
+            if index < self.getLength():
                 index += 1
             else:
                 index = 0;
-        newa = list(self.a)
-        for i in range(len(self.a)):
+        newa = list(self.__a)
+        for i in range(len(self.__a)):
             if i != index:
-                newa[i] = self.a[i]
+                newa[i] = self.__a[i]
             else:
 
                 newa[i] = value;
 
 
-        self.a = tuple(newa)
+        self.__a = tuple(newa)
 
         return self
 
     def remove(self, value):
-        index = value % len(self.a)
-        while self.a[index] != value:
-            if index < 9:
+        index = value % len(self.__a)
+        while self.__a[index] != value:
+            if index < self.getLength():
                 index += 1
             else:
                 index = 0;
-        newa = list(self.a)
+        newa = list(self.__a)
         newa[index] = None
-        self.a = tuple(newa)
+        self.__a = tuple(newa)
         return self
 
     def to_list(self):
-        return list(self.a)
+        return list(self.__a)
+
+    def from_list(self,ListExample):
+        self.clear()
+        for i in range(len(ListExample)):
+            if ListExample[i] is not  None:
+                self.add(ListExample[i])
+        return self.__a
 
     def getValue(self, num):
-        return self.a[num]
+        return self.__a[num]
 
     def isContain(self, value):
-        index = value % len(self.a)
-        while self.a[index] is not None:
-            if self.a[index] != value:
+        index = value % len(self.__a)
+        while self.__a[index] is not None:
+            if self.__a[index] != value:
                 index += 1
             else:
                 return True
         return False
+    def map(self,f):
+        temp=list(self.geta())
+        for i in range(self.getLength()):
+            if temp[i] is not None:
+                temp[i]=f(temp[i])
+        self.__a=tuple(temp)
+        return self.__a
+
+    def reduce(self,f):
+        result=0
+        for i in range(self.getLength()):
+            if self.__a[i] is not None:
+                result=f(result,self.__a[i])
+        return result
+
 
     def mempty(self):
         return None
 
     def iterator(self):
 
-        return self.a
+        return self.__a
 
 
-s = (1, None)
 
-a = HashMap()
-#print(a.add(5).a)
--- src/immutable_test.py
+++ src/immutable_test.py
@@ -6,7 +6,15 @@ from src.immutable import HashMap
 
 class TestHashMap(unittest.TestCase):
 
-    test = HashMap().geta();
+    test = HashMap(hashmaplength=10);
+
+    def test_a(self):
+        self.test.clear()
+        self.assertEqual(self.test.geta(),(None,None,None,None,None,None,None,None,None,None) )
+
+    def test_length(self):
+        self.test.clear()
+        self.assertEqual(self.test.getLength(), 10)
 
     def test_size(self):
         self.test.clear()
@@ -16,13 +24,13 @@ class TestHashMap(unittest.TestCase):
 
     def test_add(self):
         self.test.clear()
-        self.assertEquals(self.test.add(5).a,(None,None,None,None,None,5,None,None,None,None))
+        self.assertEquals(self.test.add(5).geta(),(None,None,None,None,None,5,None,None,None,None))
         self.test.clear()
-        self.assertEquals(self.test.add(5).add(6).add(16).a, (None, None, None, None, None,5, 6, 16, None, None))
+        self.assertEquals(self.test.add(5).add(6).add(16).geta(), (None, None, None, None, None,5, 6, 16, None, None))
 
     def test_remove(self):
         self.test.clear()
-        self.assertEquals(self.test.add(5).remove(5).a, (None, None, None, None, None, None, None, None, None, None))
+        self.assertEquals(self.test.add(5).remove(5).geta(), (None, None, None, None, None, None, None, None, None, None))
 
     def test_getValue(self):
         self.test.clear()
@@ -40,6 +48,25 @@ class TestHashMap(unittest.TestCase):
         self.test.clear()
         self.assertEquals(self.test.to_list(), [None,None,None,None,None,None,None,None,None,None])
         self.assertEquals(self.test.add(5).to_list(), [None,None,None,None,None,5,None,None,None,None])
+
+    def test_from_list(self):
+        self.test.clear()
+        self.assertEquals(self.test.from_list([1,3,4]), (None,1,None,3,4,None,None,None,None,None))
+
+    def test_map(self):
+        def pow2(x):
+            return x*x
+        self.test.clear()
+        self.assertEquals(self.test.add(2).add(4).map(f=pow2), (None, None, 4, None, 16, None, None, None, None, None))
+
+
+    def test_reduce(self):
+        def sum(x,y):
+            return x+y
+
+        self.test.clear()
+        self.assertEquals(self.test.add(2).add(4).reduce(f=sum), 6)
+
 
     def test_iterator(self):
         self.test.clear()
